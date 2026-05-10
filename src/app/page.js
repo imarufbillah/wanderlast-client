563704cde314bc4e2bd529e7044d3f5156c0dba6
marufbillah-dev <marufbillah.dev@gmail.com>
2026-05-10 11:05:15 +0600

    feat(hero): add Hero component with background image and search functionality

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Hero from "@/components/home/Hero";
 import Navbar from "@/components/ui/Navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
